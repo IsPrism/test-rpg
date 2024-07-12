4672865ec07fc8494542f60e60948c7495030fb0
Erik <162273639+IsPrism@users.noreply.github.com>
2024-07-12 14:52:26 -0600

    Initialize code

---
+++ code/world.js
@@ -0,0 +1,11 @@
+class Combatant {
+    constructor(team, id, level) {
+        this.combatant = document.createElement("div");
+        this.team = team
+    };
+};
+class CustomCombatant {
+    constructor(team, customStats, level) {
+
+    };
+};
