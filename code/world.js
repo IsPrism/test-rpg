e00d764c59ef66b4db3c242f6aa6065682ecf602
Erik <162273639+IsPrism@users.noreply.github.com>
2024-07-12 16:17:10 -0600

    Update

--- code/world.js
+++ code/world.js
@@ -1,7 +1,17 @@
+const combatantIDs = fetch("elementIDs/combatantIDs.json")
+console.log(combatantIDs)
+
+const playerContainer = document.getElementById("CID1");
+const enemyContainer = document.getElementById("CID2");
+
 class Combatant {
     constructor(team, id, level) {
-        this.combatant = document.createElement("div");
+        this.element = document.createElement("div");
+        playerContainer.appendChild(this.element);
+        this.icon = document.createElement("p")
+        this.element.appendChild(this.icon);
         this.team = team
+        this.icon.innerText = "";
     };
 };
 class CustomCombatant {
